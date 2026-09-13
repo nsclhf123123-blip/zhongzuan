f4050a7a478effb94cac1f7b52a61bcfbac5ee27
nsclhf123123-blip <nsclhf123123@gmail.com>
2026-09-13 13:24:24 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,21 @@
+export const config = { runtime: 'edge' };
+
 export default async function handler(req) {
   const url = new URL(req.url);
   const targetUrl = new URL("https://generativelanguage.googleapis.com");
   targetUrl.pathname = url.pathname;
   targetUrl.search = url.search;
 
+  // 过滤掉会导致转发失败的请求头
+  const headers = new Headers(req.headers);
+  headers.delete('host');
+  headers.delete('x-forwarded-host');
+  headers.delete('x-forwarded-port');
+  headers.delete('x-forwarded-proto');
+
   const newRequest = new Request(targetUrl, {
     method: req.method,
-    headers: req.headers,
+    headers: headers,
     body: req.body
   });
   const res = await fetch(newRequest);
